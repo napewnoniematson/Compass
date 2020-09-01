d18e31b0a8191f4b22bbc6946bf411db7caa4789
napewnoniematson <napewnoniematson@gmail.com>
2020-09-01 19:59:37 +0200

    add compassview and coordinatebutton

---
+++ app/src/main/java/com/napewnoniematson/compass/view/CompassView.kt
@@ -0,0 +1,9 @@
+package com.napewnoniematson.compass.view
+
+import android.content.Context
+import android.graphics.Canvas
+import android.util.AttributeSet
+import android.view.View
+import com.napewnoniematson.compass.R
+
+class CompassView(context: Context, attrs: AttributeSet) : View(context, attrs) {}
---
+++ app/src/main/java/com/napewnoniematson/compass/view/GeoCoordinateButton.java
@@ -0,0 +1,15 @@
+package com.napewnoniematson.compass.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import androidx.appcompat.widget.AppCompatButton;
+
+public class GeoCoordinateButton extends AppCompatButton{
+    public GeoCoordinateButton(Context context) {
+        super(context);
+    }
+
+    public GeoCoordinateButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
