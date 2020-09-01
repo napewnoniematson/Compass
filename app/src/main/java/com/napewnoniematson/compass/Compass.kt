003377e053876d72649a56a862801800acea4400
napewnoniematson <napewnoniematson@gmail.com>
2020-09-01 20:42:27 +0200

    basic orientation angles calculating for compass

---
+++ app/src/main/java/com/napewnoniematson/compass/Compass.kt
@@ -0,0 +1,85 @@
+package com.napewnoniematson.compass
+
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.util.Log
+
+class Compass(context: Context) : SensorEventListener {
+
+    private val TAG: String? = Compass::class.simpleName
+
+    private lateinit var sensorManager: SensorManager
+    private val accelerometerReading = FloatArray(3)
+    private val magnetometerReading = FloatArray(3)
+
+    private val rotationMatrix = FloatArray(9)
+    private val orientationAngles = FloatArray(3)
+
+    init {
+        sensorManager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    }
+
+
+    fun start() {
+        sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)?.also { accelerometer ->
+            sensorManager.registerListener(
+                this,
+                accelerometer,
+                SensorManager.SENSOR_DELAY_NORMAL,
+                SensorManager.SENSOR_DELAY_UI
+            )
+        }
+        sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)?.also { magneticField ->
+            sensorManager.registerListener(
+                this,
+                magneticField,
+                SensorManager.SENSOR_DELAY_NORMAL,
+                SensorManager.SENSOR_DELAY_UI
+            )
+        }
+    }
+
+    fun stop() {
+        sensorManager.unregisterListener(this)
+    }
+
+
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        println("Accuracy has changed. Temporary do nothing")
+    }
+
+    override fun onSensorChanged(event: SensorEvent) {
+        if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+            System.arraycopy(event.values, 0, accelerometerReading, 0, accelerometerReading.size)
+        } else if (event.sensor.type == Sensor.TYPE_MAGNETIC_FIELD) {
+            System.arraycopy(event.values, 0, magnetometerReading, 0, magnetometerReading.size)
+        }
+        updateOrientationAngles()
+        Log.d(TAG, "Orientation angles")
+        for (angle: Float in orientationAngles) {
+            Log.d(TAG, angle.toString())
+        }
+    }
+
+    fun updateOrientationAngles() {
+        // Update rotation matrix, which is needed to update orientation angles.
+        SensorManager.getRotationMatrix(
+            rotationMatrix,
+            null,
+            accelerometerReading,
+            magnetometerReading
+        )
+
+        // "mRotationMatrix" now has up-to-date information.
+
+        SensorManager.getOrientation(rotationMatrix, orientationAngles)
+
+        // "mOrientationAngles" now has up-to-date information.
+    }
+
+
+}
--- app/src/main/java/com/napewnoniematson/compass/view/MainActivity.kt
+++ app/src/main/java/com/napewnoniematson/compass/view/MainActivity.kt
@@ -2,11 +2,27 @@ package com.napewnoniematson.compass.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.napewnoniematson.compass.Compass
 import com.napewnoniematson.compass.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var compass: Compass
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        compass = Compass(this)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        compass.start()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        compass.stop()
     }
 }
