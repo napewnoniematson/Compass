ade06ac1855d1091bb68c30e60ad3e71328e3fcd
napewnoniematson <napewnoniematson@gmail.com>
2020-09-05 11:20:17 +0200

    Information toasts for wrong longitude and latitude value

--- app/src/main/java/com/napewnoniematson/compass/logic/reader/location/DestinationPointReaderImpl.kt
+++ app/src/main/java/com/napewnoniematson/compass/logic/reader/location/DestinationPointReaderImpl.kt
@@ -1,14 +1,17 @@
 package com.napewnoniematson.compass.logic.reader.location
 
+import android.app.Application
 import android.content.Context
 import android.view.View
+import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.napewnoniematson.compass.R
 import com.napewnoniematson.compass.view.widget.GeoCoordinateButton
 import com.napewnoniematson.compass.model.geo.GeoPoint
 
-class DestinationPointReaderImpl(context: Context) : DestinationPointReader, View.OnClickListener {
+class DestinationPointReaderImpl(private val context: Context) : DestinationPointReader,
+    View.OnClickListener {
 
     private val TAG: String? = DestinationPointReaderImpl::class.simpleName
 
@@ -28,12 +31,16 @@ class DestinationPointReaderImpl(context: Context) : DestinationPointReader, Vie
                 if (isLatitudeInRange(v.coordinate)) {
                     destinationPoint.latitude = v.coordinate
                     isLatitudeUpdated = true
+                } else {
+                    showLatitudeNotInRangeToast()
                 }
             }
             LONGITUDE -> {
                 if (isLongitudeInRange(v.coordinate)) {
                     destinationPoint.longitude = v.coordinate
                     isLongitudeUpdated = true
+                } else {
+                    showLongitudeNotInRangeToast()
                 }
             }
         }
@@ -46,6 +53,18 @@ class DestinationPointReaderImpl(context: Context) : DestinationPointReader, Vie
     private fun isLongitudeInRange(latitude: Float) = -180f <= latitude && latitude <= 180f
 
     private fun areCoordinatesUpdated() = isLatitudeUpdated && isLongitudeUpdated
+
+    private fun showCoordinateNotInRangeToast(messageId: Int) {
+        Toast.makeText(context, context.getString(messageId), Toast.LENGTH_LONG).show()
+    }
+
+    private fun showLatitudeNotInRangeToast() {
+        showCoordinateNotInRangeToast(R.string.latitude_not_in_range)
+    }
+
+    private fun showLongitudeNotInRangeToast() {
+        showCoordinateNotInRangeToast(R.string.longitude_not_in_range)
+    }
 
     override fun getPoint() = destinationPointLD as LiveData<GeoPoint>
 }
